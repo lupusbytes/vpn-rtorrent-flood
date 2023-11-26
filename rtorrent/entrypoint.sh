9fd133735c02f2dce0e7ac79a1d9d6b8ff7bd1b8
Bertram Baltzer Andersen <bba@delegate.dk>
2023-11-27 03:58:22 +0400

    feat(rtorrent): add incoming port variable and default rtorrent.rc

--- rtorrent/entrypoint.sh
+++ rtorrent/entrypoint.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
 echo "Deleting locks"
-rm -rf /config/.local/share/rtorrent/.session/rtorrent.lock /config/.local/share/rtorrent/.session/rtorrent.pid
+rm -rf /data/session/rtorrent.lock /data/session/rtorrent.pid
 
-if [[ ! -z "$DNS_SERVERS" ]]
-then
+if [[ ! -z "$DNS_SERVERS" ]]; then
     echo "Setting DNS server(s) to $DNS_SERVERS"
     echo $DNS_SERVERS | tr ',' '\n' | while read server; do echo "nameserver $server"; done > /etc/resolv.conf
 fi
 
-if [[ ! -z "$DEFAULT_GATEWAY" ]]
-then
+if [[ ! -z "$DEFAULT_GATEWAY" ]]; then
     OLD_DEFAULT_GATEWAY="$(ip route show default | awk '/default/ {print $3}')"
     echo "Changing default gateway from $OLD_DEFAULT_GATEWAY to $DEFAULT_GATEWAY"
     route add default gw $DEFAULT_GATEWAY dev eth0
     route del default gw $OLD_DEFAULT_GATEWAY dev eth0
 fi
 
-echo "Starting rtorrent as user: $USER"
-su "$USER" -c "rtorrent"
+if [[ ! -f "/data/rtorrent.rc" ]]; then
+    cp /etc/rtorrent/rtorrent.rc /data/rtorrent.rc
+fi
+
+echo "Setting rTorrent incoming port to $INCOMING_PORT"
+grep -q "network.port_range.set" /data/rtorrent.rc
+if [[ $? == 0 ]]; then
+    sed -i "s/network.port_range.set.*/network.port_range.set = $INCOMING_PORT-$INCOMING_PORT/g" /data/rtorrent.rc
+else
+    sed -i "$ a ## Set the incoming port" /data/rtorrent.rc
+    sed -i "$ a network.port_range.set = $INCOMING_PORT-$INCOMING_PORT" /data/rtorrent.rc
+fi
+
+chown "$PUID":"$PGID" /data/rtorrent.rc
+
+echo "Starting rTorrent as user: $USER"
+su "$USER" -c "rtorrent -D -o import=/data/rtorrent.rc -o system.daemon.set=true && sleep infinity"
